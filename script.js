74f5978fe7c4cfec4bd9575083cc10679ba2a7e4
Howard009 <43053452+Howard009@users.noreply.github.com>
2024-09-25 12:24:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var answer_1 = prompt("Will you go out with me today? (yes/no |no capitalization)");
+if (answer_1 === "yes") {
+    alert ("YAYYYYYYYYY!!! I'm going out with you!");
+    alert ("Please text me because i dont have access to this yet")
+    
+
+} else {alert ("please text me because i dont have access to this yet why you dont want to go out with me?")};
+
+
+
